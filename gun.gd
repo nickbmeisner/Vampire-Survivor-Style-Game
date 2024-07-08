b8340489ec5b3a214d3d6a1ed93056eae2e8ca38
Nick Meisner <nickbmeisner@gmail.com>
2024-07-08 16:58:10 +1000

    Updated the gun to lock onto the nearest enemy

--- gun.gd
+++ gun.gd
@@ -4,6 +4,10 @@ func _physics_process(delta):
 	var enemies_in_range = get_overlapping_bodies()
 	if enemies_in_range.size() > 0:
 		var target_enemy = enemies_in_range[0]
+		var player = get_node("../HappyBoo")
+		for enemy in enemies_in_range:
+			if (player.global_position - enemy.global_position).length() < (player.global_position - target_enemy.global_position).length():
+				target_enemy = enemy
 		look_at(target_enemy.global_position)
 		
 func shoot():
